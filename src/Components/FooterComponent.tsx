08eb408a11f9ff44cd6789470aa1a0bb8409ce4d
Ketandabhi18 <fs1.webinent@gmail.com>
2024-02-26 12:49:39 +0530

    footer

--- src/Components/FooterComponent.tsx
+++ src/Components/FooterComponent.tsx
@@ -1,65 +1,223 @@
-import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
+import {
+  FacebookOutlined,
+  Instagram,
+  LocationCityOutlined,
+  Mail,
+  Phone,
+  Telegram,
+  Twitter,
+  WhatsApp,
+  YouTube,
+} from "@mui/icons-material";
+import { Button, TextField } from "@mui/material";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
       {"Copyright © "}
-      {/* <Link color="inherit" href="https://mui.com/"> */}
       2024 Darshanyog Mahavidyalaya
-      {/* </Link>{" "} */}
       {"."}
     </Typography>
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 const FooterComponent = () => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
-        }}
-      >
-        <CssBaseline />
-        {/* <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-          <Typography variant="h2" component="h1" gutterBottom>
-            Sticky footer
-          </Typography>
-          <Typography variant="h5" component="h2" gutterBottom>
-            {"Pin a footer to the bottom of the viewport."}
-            {"The footer will move as the main element of the page grows."}
-          </Typography>
-          <Typography variant="body1">Sticky footer placeholder.</Typography>
-        </Container> */}
+    <>
+      <ThemeProvider theme={defaultTheme}>
         <Box
-          component="footer"
           sx={{
-            py: 3,
-            px: 2,
-            mt: "auto",
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "10vh",
           }}
-          style={{ backgroundColor: "rgb(183 181 179 / 82%)" }}
         >
-          <Container maxWidth="sm">
-            {/* <Typography variant="body1">
-              My sticky footer can be found here.
-            </Typography> */}
-            <Copyright />
-          </Container>
+          <CssBaseline />
+          <div style={{ display: "flex", backgroundColor: "#f7f7f7" }}>
+            <Container
+              component="main"
+              sx={{ mt: 8, mb: 2 }}
+              maxWidth="sm"
+              style={{
+                backgroundImage:
+                  "https://www.smvs.org/theme/smvs/images/world-map-dark.png",
+              }}
+            >
+              <Typography variant="h6" component="h1" gutterBottom>
+                Contact Us
+              </Typography>
+              <span
+                style={{
+                  display: "flex",
+                  paddingTop: "10px",
+                  alignItems: "center",
+                  alignContent: "center",
+                }}
+              >
+                <LocationCityOutlined />
+                <p style={{ paddingLeft: "15px", fontSize: "smaller" }}>
+                  आर्यवन, रोजड़, पत्रा.: सागपुर, तालुका : तलोद, जिला : साबरकांठा,
+                  गुजरात- 383307
+                </p>
+              </span>
+              <span
+                style={{
+                  display: "flex",
+                  paddingTop: "10px",
+                  alignItems: "center",
+                  alignContent: "center",
+                }}
+              >
+                <Phone />
+                <p style={{ paddingLeft: "15px", fontSize: "smaller" }}>
+                  9409615011, 9409415011
+                </p>
+              </span>
+
+              <span
+                style={{
+                  display: "flex",
+                  paddingTop: "10px",
+                  alignItems: "center",
+                  alignContent: "center",
+                }}
+              >
+                <Mail />
+                <p style={{ paddingLeft: "15px", fontSize: "smaller" }}>
+                  darshanyog@gmail.com
+                </p>
+              </span>
+              <span
+                style={{
+                  display: "flex",
+                  paddingTop: "2%",
+                }}
+              >
+                <div
+                  style={{
+                    border: "2px",
+                    height: "25px",
+                    width: "25px",
+                    margin: "15px",
+                  }}
+                >
+                  <FacebookOutlined style={{}} />
+                </div>
+                <div
+                  style={{
+                    border: "2px",
+                    height: "30px",
+                    width: "20px",
+                    margin: "15px",
+                  }}
+                >
+                  <Instagram style={{}} />
+                </div>
+                <div
+                  style={{
+                    border: "2px",
+                    height: "30px",
+                    width: "20px",
+                    margin: "15px",
+                  }}
+                >
+                  <Twitter style={{}} />
+                </div>
+                <div
+                  style={{
+                    border: "2px",
+                    height: "30px",
+                    width: "20px",
+                    margin: "15px",
+                  }}
+                >
+                  <YouTube style={{}} />
+                </div>
+                <div
+                  style={{
+                    border: "2px",
+                    height: "30px",
+                    width: "20px",
+                    margin: "15px",
+                  }}
+                >
+                  <WhatsApp style={{}} />
+                </div>
+                <div
+                  style={{
+                    border: "2px",
+                    height: "30px",
+                    width: "20px",
+                    margin: "15px",
+                  }}
+                >
+                  <Telegram style={{}} />
+                </div>
+              </span>
+            </Container>
+            <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+              <Typography variant="h6" component="h1" gutterBottom>
+                Message for Us
+              </Typography>
+              <div style={{ display: "grid", width: "60%" }}>
+                <TextField
+                  label="Name"
+                  variant="standard"
+                  style={{ marginBottom: "2%" }}
+                  placeholder="Enter Name"
+                  // defaultValue="Hello World"
+                />
+
+                <TextField
+                  label="Email"
+                  variant="standard"
+                  style={{ marginBottom: "2%" }}
+                  placeholder="Enter Email"
+                  // defaultValue="Hello World"
+                />
+
+                <TextField
+                  label="Mobile"
+                  variant="standard"
+                  style={{ marginBottom: "2%" }}
+                  placeholder="Enter Mobile"
+                  // defaultValue="Hello World"
+                />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "2%",
+                  }}
+                >
+                  <Button variant="outlined">Outlined</Button>
+                </div>
+              </div>
+            </Container>
+          </div>
+          <Box
+            component="footer"
+            sx={{
+              py: 3,
+              px: 2,
+              mt: "auto",
+            }}
+            style={{ backgroundColor: "rgb(183 181 179 / 82%)" }}
+          >
+            <Container maxWidth="sm">
+              <Copyright />
+            </Container>
+          </Box>
         </Box>
-      </Box>
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   );
 };
 
