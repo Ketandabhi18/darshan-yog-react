194e5cc1dabfad3fd964c74563ad74c86af1636b
Ketandabhi18 <fs1.webinent@gmail.com>
2024-03-20 17:19:44 +0530

    login with otp

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,32 +3,28 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Loader from "../Components/Loader/Loader";
-import AuthService from "../api/auth/auth";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Login: FunctionComponent = () => {
+  const [step, setStep] = useState<any>(false);
+  const [mobile, setMobile] = useState<any>();
+  const [otp, setOtp] = useState<any>("");
   const [loader, setLoader] = useState<boolean>(false);
   const navigate = useNavigate();
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleVerfiyOtp = async (event: any) => {
     event.preventDefault();
     setLoader(true);
     try {
-      const data = new FormData(event.currentTarget);
-      const username: any = data.get("email");
-      const password = data.get("password");
-
+      const username: any = mobile;
+      const password = otp;
       axios
+        // .post("http://localhost:7001/login", {
         .post("https://darshan-yog-node-apis.onrender.com/login", {
           username,
           password,
@@ -46,28 +42,7 @@ const Login: FunctionComponent = () => {
             localStorage.setItem(
               "userDetail",
               JSON.stringify({
-                mobileNumber: "+919998982350",
-                countrycode: "+91",
-                email: "fs1ad@gmail.com",
-                role: "ROLE_USER",
-                firstName: "Raj",
-                middleName: null,
-                lastName: null,
-                whatsappNumber: "",
-                gender: "Male",
-                dateOfBirth: "07-05-1994",
-                edQualification: "B.E",
-                profession: "Software engineer",
-                guardianName: null,
-                maritalStatus: "Unmarried",
-                bloodGroup: "B+",
-                addrLine1: "Ahmedabad",
-                addrLine2: "science city",
-                city: "Ahmedabad",
-                district: "Ahmedabad",
-                state: "Gujarat",
-                country: "india",
-                pincode: "380052",
+                ...userDetails,
               })
             );
             navigate("/");
@@ -80,7 +55,22 @@ const Login: FunctionComponent = () => {
       console.log(error);
     }
   };
-
+  const handleGetOtp = async () => {
+    try {
+      console.log("mobile :: in get otp ", mobile);
+      setStep(true);
+      const { data } = await axios.post(
+        // "http://localhost:7001/get-otp",
+        "https://darshan-yog-node-apis.onrender.com/get-otp",
+        {
+          username: mobile,
+        }
+      );
+      console.log("data :: get otp :: response", data);
+    } catch (error) {
+      console.log("error :: ", error);
+    }
+  };
   const defaultTheme = createTheme();
 
   return (
@@ -103,45 +93,34 @@ const Login: FunctionComponent = () => {
             <Typography component="h1" variant="h5">
               Log in
             </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Mobile Number"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              {/* <FormControlLabel
+            {step === false && (
+              <Box sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Mobile Number"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={(e) => setMobile(e.target.value)}
+                />
+
+                {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             /> */}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Log In
-              </Button>
-              {/* <Grid container>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  onClick={handleGetOtp}
+                >
+                  Get Otp
+                </Button>
+                {/* <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
@@ -153,7 +132,46 @@ const Login: FunctionComponent = () => {
                 </Link>
               </Grid>
             </Grid> */}
-            </Box>
+              </Box>
+            )}
+            {step && (
+              <>
+                <Box sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    disabled
+                    fullWidth
+                    id="email"
+                    label="Mobile Number"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    value={mobile}
+                  />
+                  <TextField
+                    margin="normal"
+                    onChange={(e) => setOtp(e.target.value)}
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={handleVerfiyOtp}
+                  >
+                    Verify Otp
+                  </Button>
+                </Box>
+              </>
+            )}
           </Box>
           {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
         </Container>
